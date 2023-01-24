b61a51b663714d6156e591c7c9d7f59ef42d5bef
Ronald <rvpostha15@yahoo.com>
2023-01-24 17:12:03 -0500

    rearranged NavBar, created new Parent for Study Component, differentiated My Flashcards from Study

--- flashcard_phase_2_project/src/components/Study.js
+++ flashcard_phase_2_project/src/components/Study.js
@@ -1,11 +1,28 @@
+import React, {useState} from 'react';
 
-function Study () {
+function Study ({card}) {
+    const [isFront, setIsFront]=useState(true)
+
+    const toggleStudy = () => {    
+        setIsFront(isFront => !isFront)
+    }
+
+    const {id, title, question, answer, image} = card
+
     return (
-        <div>
-            <h1>Study</h1>
+        <div className='container'  onClick={toggleStudy}>
+            <div className='card'>
+                {isFront? (
+                <h2 className="front">{title}
+                 <div> {question}</div>
+                  </h2>
+                ) : (
+                <h2 className="back">{answer}<img src={image}/></h2> 
+                )}
+            </div>
         </div>
-    )
-}
+    );
+  }
 
 
 export default Study
